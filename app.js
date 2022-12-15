7e218e04995d07d8ee4dd792ad814720199d0ffe
Nagraj Devadiga <34651415+ndevadiga1406@users.noreply.github.com>
2022-12-15 21:02:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+
+//  NavBar Active class
+$(document).on('click', 'ul li', function () {
+    $(this).addClass('active').siblings().removeClass('active')
+});
+
+function contactSubmit(form){
+    // On Submit code
+}
